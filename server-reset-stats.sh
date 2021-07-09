86de945c769137568c542992861c99e2e84b824a
cyberpoolorg <86299091+cyberpoolorg@users.noreply.github.com>
2021-07-09 23:15:22 +0200

    Update server-reset-stats.sh

--- server-reset-stats.sh
+++ server-reset-stats.sh
@@ -21,7 +21,7 @@ echo ""
 
 redis-cli DEL statHistory
 
-~/.nvm/versions/node/v12.13.0/bin/pm2 start --name pool node -- --optimize_for_size --max-old-space-size=4096 "${BASEDIR}/init.js"
+~/.nvm/versions/node/v8.17.0/bin/pm2 start --name pool node -- --optimize_for_size --max-old-space-size=4096 "${BASEDIR}/init.js"
 
 renice -n -18 -p $(pidof node)
 renice -n -18 -p $(pidof nodejs)
