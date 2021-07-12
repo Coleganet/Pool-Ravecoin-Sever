4c2f4cb89d85eb198bbedb62ea6db83de4304465
cyberpoolorg <86299091+cyberpoolorg@users.noreply.github.com>
2021-07-12 18:12:57 +0200

    Update server-reset-stats.sh

--- server-reset-stats.sh
+++ server-reset-stats.sh
@@ -23,8 +23,8 @@ redis-cli DEL statHistory
 
 ~/.nvm/versions/node/v12.13.0/bin/pm2 start --name pool node -- --optimize_for_size --max-old-space-size=4096 "${BASEDIR}/init.js"
 
-renice -n -18 -p $(pidof node)
-renice -n -18 -p $(pidof nodejs)
+sudo renice -n -18 -p $(pidof node)
+sudo renice -n -18 -p $(pidof nodejs)
 
 echo ""
 echo "Stats Cleared Ravencoin Server Restarted!"
