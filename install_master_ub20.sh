a365380962c6dda0bd17771ff0e4af43b732c7b9
cyberpoolorg <86299091+cyberpoolorg@users.noreply.github.com>
2021-07-12 16:37:23 +0200

    Update install_master_ub20.sh

--- install_master_ub20.sh
+++ install_master_ub20.sh
@@ -44,8 +44,8 @@ sudo chown -R $USER:$GROUP ~/.nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install v12.22.3
-nvm use v12.22.3
+nvm install v12.22.2
+nvm use v12.22.2
 
 npm update -g
 npm install -g pm2@5.1.0
