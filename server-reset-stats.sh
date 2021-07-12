34a1f983a8389f78740d21e441145a91f9fc2a15
cyberpoolorg <86299091+cyberpoolorg@users.noreply.github.com>
2021-07-12 16:39:10 +0200

    Update server-reset-stats.sh

--- server-reset-stats.sh
+++ server-reset-stats.sh
@@ -17,7 +17,7 @@ echo "Current working dir : $PWD"
 echo "Script location path (dir) : $BASEDIR"
 echo ""
 
-~/.nvm/versions/node/v12.13.0/bin/pm2 del pool
+~/.nvm/versions/node/v12.22.2/bin/pm2 del pool
 
 redis-cli DEL statHistory
 
