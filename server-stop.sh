c4d487d7c3798480919787bd4ba4e7718fe402c2
cyberpoolorg <86299091+cyberpoolorg@users.noreply.github.com>
2021-08-31 18:39:20 +0200

    Update server-stop.sh

--- server-stop.sh
+++ server-stop.sh
@@ -17,7 +17,7 @@ echo "Current working dir : $PWD"
 echo "Script location path (dir) : $BASEDIR"
 echo ""
 
-~/.nvm/versions/node/v12.22.5/bin/pm2 stop pool
+~/.nvm/versions/node/v12.22.6/bin/pm2 stop pool
 
 echo ""
 echo "Ravencoin Server Stopped!"
